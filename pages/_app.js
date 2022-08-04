70b2c68de69268a80cf910c35085c8d4298a1498
maher <gomemahero.com>
2022-08-04 13:28:31 +0300

    register with  mongodb data send

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "../components/Nav";
 import Head from "next/head";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "antd/dist/antd.css";
+import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="/css/styles.css" />
-      </Head>
-      <Nav />
-      <Component {...pageProps} />
-    </>
+    <Head>
+      
+    </Head>
+    <Nav />
+    <ToastContainer position="top-center" />
+    <Component {...pageProps} />
+  </>
   );
 }
 
--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import {toast} from 'react-toastify'
 
 const Register = () => {
   const [name, setName] = useState("");
